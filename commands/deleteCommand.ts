701e8a2e71dc4419eb4ea426fad43067c5e6b3af
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-10-04 09:52:11 -0300

    refactor: move Delete Command to a separate file

---
+++ commands/deleteCommand.ts
@@ -0,0 +1,36 @@
+import fs from 'node:fs/promises';
+
+import StdError from '../error';
+import ICommand from '../ICommand';
+
+interface DeleteParams {
+  path: string;
+}
+
+export default class DeleteCommand implements ICommand<DeleteParams> {
+  private _regex: RegExp = /^delete the file\s+(.+)$/;
+
+  public get regex(): RegExp {
+    return this._regex;
+  }
+
+  matchPattern(commandString: string): DeleteParams | null {
+    const match = commandString.match(this._regex);
+    if (match) {
+      const path = match[1];
+      return { path };
+    }
+    return null;
+  }
+
+  async action(params: DeleteParams): Promise<void> {
+    const { path } = params;
+    try {
+      await fs.unlink(path);
+      console.log(`File from path ${path} deleted successfully`);
+    } catch (error) {
+      if (error instanceof StdError && error.code === 'ENOENT') return;
+      console.error(error);
+    }
+  }
+}
