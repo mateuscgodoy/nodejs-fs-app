8b3f5b7d2bfca35186fb41f5a43ccbdf35f64afc
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-10-04 09:44:36 -0300

    fix: add export as default and removed unused imports from Create Command

--- commands/createCommand.ts
+++ commands/createCommand.ts
@@ -1,13 +1,12 @@
 import fs from 'node:fs/promises';
 
-import StdError from '../error';
 import ICommand from '../ICommand';
 
 interface CreateParams {
   path: string;
 }
 
-export class CreateCommand implements ICommand<CreateParams> {
+export default class CreateCommand implements ICommand<CreateParams> {
   private _regex: RegExp = /^create the file\s+(.+)$/;
 
   public get regex(): RegExp {
